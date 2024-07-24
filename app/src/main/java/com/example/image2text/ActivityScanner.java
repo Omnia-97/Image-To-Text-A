2620a1a778ffedeee894d49b605c8fa3d812bd42
DELL <omnianabil1920@gmail.com>
2024-07-24 15:04:05 +0300

    implement take a snap

--- app/src/main/java/com/example/image2text/ActivityScanner.java
+++ app/src/main/java/com/example/image2text/ActivityScanner.java
@@ -1,17 +1,22 @@
 package com.example.image2text;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ActivityScanner extends AppCompatActivity {
     private ImageView captureIV;
@@ -24,7 +29,6 @@ public class ActivityScanner extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
-
         captureIV=findViewById(R.id.idIVCaptureImage);
         resultTV=findViewById(R.id.idTVDetectedText);
         snapBtn=findViewById(R.id.idButtonSnap);
@@ -54,10 +58,41 @@ public class ActivityScanner extends AppCompatActivity {
         return cameraPermission == PackageManager.PERMISSION_GRANTED;
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(grantResults.length >0){
+
+            boolean cameraPermission = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            if(cameraPermission){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                captureImage();
+            }else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+
+        }    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            captureIV.setImageBitmap(imageBitmap);
+        }
+    }
+
     private void detectText(){
 
     }
     private void captureImage(){
+        Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(takePicture.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(takePicture,REQUEST_IMAGE_CAPTURE);
+        }
 
     }
     private void requestPermission(){
