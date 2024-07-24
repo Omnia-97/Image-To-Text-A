58ba398bc323e08a712def5f78df05fd551442b5
DELL <omnianabil1920@gmail.com>
2024-07-24 14:46:30 +0300

    implement camera permission

--- app/src/main/java/com/example/image2text/ActivityScanner.java
+++ app/src/main/java/com/example/image2text/ActivityScanner.java
@@ -1,14 +1,71 @@
 package com.example.image2text;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ActivityScanner extends AppCompatActivity {
+    private ImageView captureIV;
+    private TextView resultTV;
+    private Button snapBtn, detectBtn;
+    private Bitmap imageBitmap;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
+
+        captureIV=findViewById(R.id.idIVCaptureImage);
+        resultTV=findViewById(R.id.idTVDetectedText);
+        snapBtn=findViewById(R.id.idButtonSnap);
+        detectBtn=findViewById(R.id.idButtonDetect);
+        detectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                detectText();
+            }
+        });
+
+        snapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(checkPermission()){
+                    captureImage();
+                }else{
+                    requestPermission();
+                }
+            }
+        });
+    }
+    private boolean checkPermission(){
+        int cameraPermission= ContextCompat.checkSelfPermission(getApplicationContext(),CAMERA_SERVICE);
+        return cameraPermission == PackageManager.PERMISSION_GRANTED;
+
+    }
+    private void detectText(){
+
+    }
+    private void captureImage(){
+
+    }
+    private void requestPermission(){
+        int permissionCode = 200;
+        ActivityCompat.requestPermissions(this , new String[]{
+                Manifest.permission.CAMERA
+        },
+                permissionCode);
+
     }
 }
